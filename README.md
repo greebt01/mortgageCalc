d8aa43ff066c63bf20994d43d3dd30b5d4c105dc
Birk <rgeebt01@yahoo.com>
2022-04-20 17:05:46 -0400

    added error checking

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 # Description
 Mortgage calculator
+
+# Update
+Added error checking with while and if loops
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What is the amount of principal?");
-        Double p = scanner.nextDouble();
-        System.out.print("What is the annual interest rate?");
-        Double r = scanner.nextDouble()/100/12;
-        System.out.print("What is the term in years?");
-        Integer n = scanner.nextInt()*12;
-        Double myMort = p * ((r * Math.pow(1 + r, n))/ (Math.pow(1 + r, n) - 1));
+        double p = 0;
+        double r = 0;
+        int n = 0;
+
+        while (true) {
+            System.out.print("What is the amount of principal? ($1K - $1M)");
+            p = scanner.nextDouble();
+            if (p>=1_000 && p<=1_000_000) {
+                break;
+            }
+            System.out.print("Enter a number between 1000 and 1000000");
+        }
+        while (true) {
+            System.out.print("What is the annual interest rate? (0% - 30%)");
+            r = scanner.nextDouble()/100/12;
+            if (r>=0&&r<=30) {
+                break;
+            }
+            System.out.println("Enter a number between 0 and 30");
+        }
+        while (true) {
+            System.out.print("What is the term in years? (1year - 50years)");
+            n = scanner.nextInt();
+            if (n>=1&&n<=50) {
+                break;
+            }
+            System.out.println("Enter a number between 1 and 50");
+        }
+
+        Double myMort = p * ((r * Math.pow(1 + r, n*12))/ (Math.pow(1 + r, n*12) - 1));
         NumberFormat cash = NumberFormat.getCurrencyInstance();
-        System.out.println("Your mortgage payment would be " + cash.format(myMort) + " per month for " + n + " months.");
+        System.out.println("Your mortgage payment would be " + cash.format(myMort) + " per month for " + n*12 + " months.");
     }
 }
